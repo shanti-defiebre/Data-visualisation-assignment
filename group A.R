3ab9cfd4629c76467ab69444db5c11e4530e4142
shanti-defiebre <bt21409@qmul.ac.uk>
2024-03-28 18:01:14 +0000

    simplidied it, now we have the data in files

--- group A.R
+++ group A.R
@@ -5,6 +5,8 @@ install.packages("tidyverse")
 library(tidyverse)
 install.packages("readr")
 library(readr)
-spotify_songs <- tibble(readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv'))
 
-View(spotify_songs)
+spotify <- read.csv("spotify_data.csv")
+View(spotify)
+
+
